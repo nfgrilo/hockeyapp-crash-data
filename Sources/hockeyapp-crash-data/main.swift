55f9e1447060cd3fa094edc6be634ad44c8e68f6
Nuno Grilo <nfgrilo@gmail.com>
2019-01-29 11:50:05 +0000

    Require appID argument as well

--- Sources/hockeyapp-crash-data/main.swift
+++ Sources/hockeyapp-crash-data/main.swift
@@ -3,8 +3,9 @@ import Commander
 
 // fetch crash data
 let fetchCommand = command(
-    Argument<String>("token", description: "The HockeyApp Token")
-    ) { token in
-        HockeyAppCrashDataFetcher(token: token).fetchCrashDataPerUser()
+    Argument<String>("token", description: "The HockeyApp Token"),
+    Argument<String>("appID", description: "The unique identifier for the HockeyApp application")
+    ) { token, appID in
+        HockeyAppCrashDataFetcher(token: token, appID: appID).fetchCrashDataPerUser()
     }
 fetchCommand.run()
